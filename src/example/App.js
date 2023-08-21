a2c202d0138d56eae8f16267175255b688e5bdd7
sandipan.thorat <sandipan.thorat@tredence.com>
2023-08-21 14:16:52 +0530

    change folder structure

--- src/example/App.js
+++ src/example/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from "react-dom";
-import { TextInput } from "./lib";
+import TextInput from 'st_charts';
+// import { TextInput } from "../lib"
 
 const App = () => (
     <div style={{ width: 640, margin: "15px auto" }}>
         <h1>Hello Sandipan</h1>
-        <TextInput label="Email Address" placeholder="name@example.com" />
+        <TextInput label="Email Address" placeholder="sandipan@example.com" />
     </div>
 );
 
